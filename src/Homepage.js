63d9521b89b751316ceb4792afbf568bb05ec121
kkv263 <kkv263@utexas.edu>
2018-06-13 15:36:28 -0500

    progress on info pages

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-import {HomepageWrapper, Portrait, Image, ContentWrapper, Pname, Poverlay, InformationWindow} from './styles/Homepage.style';
+import {HomepageWrapper, Portrait, Image, ContentWrapper, 
+  Pname, Poverlay, InformationWindow, InformationText, 
+  TabWrapper, Tab, InformationHeader} from './styles/Homepage.style';
 
 class Homepage extends Component {
   constructor(){
     super();
+    this.state = {
+      activeTab: 0
+    };
+
   }
 
-  componentDidMount(){
-
+  handleTab(value){
+    var updatedArray = [false, false];
+    updatedArray[value] = true;
+    this.setState({ activeTab: value});
   }
 
   render() {
+    var activeTab = this.state.activeTab;
+
+    var showTabs;
+
+    switch(activeTab){
+      case 0:
+        showTabs = (
+        <InformationText>
+          <InformationHeader>General:</InformationHeader>
+          Jin is a well rounded character, with a good parry, good pokes, a wavedash, 
+          frametraps, CH pokes, as well as just slightly inferior versions of 
+          Mishima staples like the EWGF and hellsweep.
+          ↓↘→①②③④
+
+        <InformationHeader>Pros:</InformationHeader>
+          Good selection of pokes with good range. All around solid. Good use of parry can raise his potential. Simple to learn and use barring executional requirements
+
+        <InformationHeader>Cons:</InformationHeader>
+          Low to average damage in the open field.
+        </InformationText>);
+        break;
+      default:
+      break;
+    }
+
+
     return (
       <HomepageWrapper>
       <ContentWrapper>
@@ -22,7 +56,14 @@ class Homepage extends Component {
 
         </Portrait>
       <InformationWindow>
-        yo 
+
+        <TabWrapper>
+          <Tab active = {activeTab === 0} onClick={() => this.handleTab(0)}>Playstyle</Tab> 
+          <Tab active = {activeTab === 1} onClick={() => this.handleTab(1)}>Top 10 Moves</Tab> 
+          <Tab active = {activeTab === 2} onClick={() => this.handleTab(2)}>Frames</Tab> 
+        </TabWrapper>
+        {showTabs} 
+
       </InformationWindow>
       </ContentWrapper>
 
--- src/styles/Homepage.style.js
+++ src/styles/Homepage.style.js
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 export const HomepageWrapper = styled.div`
-position:absolute;
+position:relative;
 width:100%;
 height: 100vh;
 background: linear-gradient(to bottom, #37596b 0%, #0e1c23 100%);
 `
 
 export const ContentWrapper = styled.div`
-margin-left: 2.5%;
-margin-top: 10%;
-margin-right: 2.5%;
-background:blue;
+position:absolute;
+bottom:5%;
+left: 2.5%;
+right: 2.5%;
 `
 
 export const Image = styled.img`
-max-width: 100%;
-height: auto;
+width: 100%;
+height:auto;
 `
 
 export const Poverlay = styled.div`
@@ -24,7 +24,6 @@ position:absolute;
 left:0;
 bottom:0;
 width:100%;
-height: 45px;
 background: rgba(0,0,0,.5);
 text-align:center;
 vertical-align: middle;
@@ -42,15 +41,53 @@ export const Portrait = styled.div`
 float:left;
 position:relative;
 width:40%;
-border: 1px 1px 1px 0px solid #0D485D;
 background:#091722;
-border-radius: 2px;
 box-shadow: 0 0 10px 10px #0D485D inset;
 `
 
 export const InformationWindow = styled.div`
 float:right;
 width:57.5%;
-height:auto;
-background: red;
+background:#091722;
+box-shadow: 0 0 10px 10px #0D485D inset;
+`
+
+export const TabWrapper = styled.div`
+width:95%;
+height: 30px;
+margin-top: 2%;
+margin-left: 2.5%;
+margin-right: 2.5%;
+`
+
+export const Tab = styled.div`
+font-family: 'Orbitron', sans-serif;
+font-size: 1.3em;
+color: ${props => props.active ? '#fff' : '#37596b'};
+display:inline-block;
+width: 33.33%;
+text-align:center;
+border-bottom: 2px solid ${props => props.active ? '#fff' : 'none'};
+transition: border-color linear .25s, color linear .25s;
+text-shadow: 0 0 2px ${props => props.active ? '#fff' : 'none'};
+
+&:hover {
+  color: #fff;
+  border-bottom: 2px solid #fff;
+  cursor:pointer;
+}
+`
+
+export const InformationHeader = styled.h1`
+font-family: 'Orbitron', sans-serif;
+font-size: 1.1em;
+margin-top: 2%;
+`
+
+export const InformationText = styled.p`
+font-family: 'Questrial', sans-serif;
+color: white;
+font-size: 1.2em;
+line-height: 1.3em;
+margin: 2% 2% 2% 2%;
 `
